0cdc6d0e1697637cf925ad1f6e46e33e2f6e1f32
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-04-27 02:03:04 +0200

    added first script hub

--- RandomTP.lua
+++ RandomTP.lua
@@ -17,19 +17,26 @@ local Notification = NEVERLOSE:Notification()
 local Window = NEVERLOSE:AddWindow("ZBOY HUB", "TROLL SCRIPTS - NEXT GENERATION")
 
 local Tab = Window:AddTab("Main","earth")
+local Hubs = Window:AddTab("Script Hubs", "list")
 
-local MassTp = Tab:AddSection("MASS TP")
-local DestroyGui = Tab:AddSection("Destroy GUI")
+local MassTp = Tab:AddSection("MASS TP", "left")
+local DestroyGui = Tab:AddSection("Destroy GUI", "right")
 
 MassTp:AddToggle("Random TP", false, function(val)
     Active=val
 end)
-MassTp:AddSlider("Distance",0,100,15,function(val)
+MassTp:AddSlider("Distance",0,50,15,function(val)
     Distance=val
 end)
 
 DestroyGui:AddButton("Destroy GUI", function()
     Window:Delete()
+end)
+
+local HubsSection = Hubs:AddSection("Script Hubs")
+
+HubsSection:AddButton("Sky Hub FE", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/FE%20Trolling%20GUI.luau"))
 end)
 
 function Update()
